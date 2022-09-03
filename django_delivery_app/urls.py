6c6519d065ba73726b81de95bea887a658fc30aa
David Ukelere <david.ukelere291@gmail.com>
2022-09-03 06:35:34 +0100

    Setup basic homepage

--- django_delivery_app/urls.py
+++ django_delivery_app/urls.py
@@ -20,4 +20,6 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('customer', views.customer_page),
+    path('courier', views.courier_page),
 ]
